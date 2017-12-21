21316424ba6b83df5be78e4b9730beb65f82e8ac
alex-veikin <vejkin@yandex.ru>
2017-12-21 19:29:52 +0300

    Add js_13-coursework at home

---
+++ js_12/js/main.js
@@ -0,0 +1,37 @@
+// //Конструкторы
+//
+// var Student = function (name, age) {
+//   this.name = name;
+//   this.age = age;
+// };
+//
+// var student1 = new Student("John", 25);
+//
+// console.log(student1.name);
+// console.log(student1);
+
+
+// var button = document.querySelectorAll("button");
+//
+// for(var i = 0; i < button.length; i++) {
+//     button[i].onclick = function() {
+//         alert(this.innerHTML);
+//     }
+// }
+//
+//
+// var Car = {
+//   constructor: function (name, velocity, color) {
+//       this.name = name;
+//       this.velocity = velocity;
+//       this.color = color;
+//       return this;
+//   },
+//     go: function () {
+//       console.log(this.name + " run with " + this.velocity + "mph");
+//     }
+// };
+//
+// var auto1 = Object.create(Car).constructor("BMW", 200, "Black");
+//
+// auto1.go();
