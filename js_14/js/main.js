55a03cf60f86681bf4faa9c760bf20439a886f97
alex-veikin <vejkin@yandex.ru>
2017-12-22 20:08:46 +0300

    Fix js_12 carousel

--- js_14/js/main.js
+++ js_14/js/main.js
@@ -7,32 +7,32 @@ $(function () {
 
         var i = 0;
 
-        var slider = this;
+        // var slider = this;
 
         this.prev = function () {
-            slider.images.eq(i).removeClass('showed');
+            slider.images[i].classList.remove('showed');
             i--;
 
             if (i < 0) {
                 i = slider.images.length - 1;
             }
 
-            slider.images.eq(i).addClass('showed');
+            slider.images[i].classList.add('showed');
         };
 
         this.next = function () {
-            slider.images.eq(i).removeClass('showed');
+            slider.images[i].classList.remove('showed');
             i++;
 
             if (i >= slider.images.length) {
                 i = 0;
             }
 
-            slider.images.eq(i).addClass('showed');
+            slider.images[i].classList.add('showed');
         };
 
-        slider.btnPrev.click(slider.prev);
-        slider.btnNext.click(slider.next);
+        document.querySelector(slider.btnPrev).onclick = slider.prev;
+        document.querySelector(slider.btnNext).onclick = slider.next;
     }
 
     var slider = new Slider();
